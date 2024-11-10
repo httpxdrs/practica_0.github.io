c6c86a2e7718dde713a9c401483cc8ddc1e197fb
httpxdrs <22690201@tecvalles.mx>
2024-11-10 14:44:26 -0600

    avance 7

--- Portafolio/carpetaHTML/Js/Script.js
+++ Portafolio/carpetaHTML/Js/Script.js
@@ -56,3 +56,36 @@ if (edad >= 18) {
      default:
      console.error("Día no válido");
      }
+
+     let multiplicacion = (a, b) => a * b;
+     console.log(multiplicacion(5, 3)); // 15
+
+
+     let objetoo = {
+       nombre: "Jaime",
+       saludar: function () {
+       setTimeout(() => {
+       console.log(`Hola, soy ${this.nombre}`);
+       }, 1000);
+       },
+       };
+      
+       objetoo.saludar();
+
+       let arreglo = new Array(1, 2, 3, 4, 5);
+       let colores = ["rojo", "verde", "azul"];
+
+       let frutas = ["🍎", "🍐","🍇","🍓"]; 
+         frutas.push("🥭");
+         frutas.unshift("🍌");
+         frutas.pop();
+         console.log(frutas); // 🍌, 🍎, 🍐, 🍇, 🍓
+        
+         let frutas2 = frutas.slice(1, 3);
+         console.log(frutas2.join(" - ")); // 🍎 - 🍐
+         console.log(frutas.length); // 5
+         console.log(frutas.indexOf("🍇")); // 3
+        
+         let frutas3 = frutas.map((fruta) => fruta +
+        "🍉");
+         console.table(frutas3);
